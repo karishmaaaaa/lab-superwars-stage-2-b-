926390b970cb5500c8adf87274e5e750808dcce7
karishmaaaaa <karishma.ss@kalvium.community>
2023-10-20 11:10:37 +0530

    DOne

--- src/app.js
+++ src/app.js
@@ -26,7 +26,25 @@ const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
-
+    for (let i =0; i < players.length; i++){
+       
+        if (i%2==0){
+            detailedPlayers.push({
+                name : players[i],
+                strength : getRandomStrength(),
+                image : `./images/super-${i+1}.png`,
+                type : "hero"
+            })
+        }else{
+            detailedPlayers.push({
+                name : players[i],
+                strength : getRandomStrength(),
+                image : `./images/super-${i+1}.png`,
+                type : "villain"
+            })
+        }
+    
+    }
     return detailedPlayers;
 }
 
@@ -34,6 +52,7 @@ const initPlayers = (players) => {
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
+    return Math.floor(Math.random()*101)
 }
 
 const buildPlayers = (players, type) => {
@@ -42,7 +61,15 @@ const buildPlayers = (players, type) => {
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
-
+    for (var j=0;j<players.length;j++) {
+        if (players[j].type == type){
+            fragment += `<div class="player">
+                <img src="${players[j].image}" alt="">
+                <div class="name">${players[j].name}</div>
+                <div class="strength">${players[j].strength}</div>
+                </div>`;
+        }
+    }
     return fragment;
 }
 // Display players in HTML
